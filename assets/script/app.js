e0e9dfe21f3a7d08da0d3c7a83cf0b5855d18352
Bernard <oyebenard@gmail.com>
2023-11-08 11:50:31 -0600

    Calculation functions added

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,47 @@
 'use strict';
+// Utility functions
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}  
+  
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+// Main 
+const height = select('.height');
+const weight = select('.weight');
+const result = select('.result span');
+const calculate = select('.calculate');
+const response = select('.result');
+
+onEvent('load', window, () => {
+    height.value = '';
+    weight.value = '';
+});
+
+function getBmi(heightnum, weightnum) {
+    let bmi = weightnum * 703/ (heightnum * heightnum);
+    return bmi;
+}
+
+function isValid(...arg) {
+    if (arg !== '' && !isNaN(Number.parseFloat(arg))) {
+        return true;
+    }
+    else {
+        result.innerText = "Please, enter a valid height and weight";
+    }
+}
+
+onEvent('click', calculate, () => {
+    let heightNum = Number.parseFloat(height.value);
+    let weightNum = Number.parseFloat(weight.value);
+
+    if (isValid(heightNum, weightNum)) {
+        let bmi = getBmi(heightNum, weightNum);
+        result.innerText = `${bmi.toFixed(1)}`;
+    }
+});
+
+
